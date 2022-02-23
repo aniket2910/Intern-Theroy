ad8ba1c10cbbdf630e91488710887996c86c98de
Debobrota Haldar <99647439+iamdebobrota@users.noreply.github.com>
2022-02-23 14:40:22 +0530

    login signup and contact page
    complete

--- Script/login.js
+++ Script/login.js
@@ -19,15 +19,14 @@ document.querySelector("#company").addEventListener("click", function () {
   company.style.color = "blue";
   document.querySelector("#lbox").innerHTML =
     `If you are a student looking for internships, please login as a student by clicking <a href="signup.html"> here.</a>`;
-  // document.querySelector(".facebook").remove;
+
+
 
 
   document.querySelector(".facebook").style.display="none";
   document.querySelector(".google").style.display="none";
   document.querySelector(".linkdin").style.display="block";
 
-  // document.querySelector(".google").;
-  // document.createElement(".linkdin").window.location.href="https://www.linkedin.com/uas/login?"
 });
 
 document.querySelector(".here").addEventListener("click", function () {
@@ -56,37 +55,3 @@ function myFormSubmit() {
       window.location.href = "index.html";
     });
 }
-
-// var regdUsers = JSON.parse(localStorage.getItem("signUpDetails"));
-// console.log(regdUsers);
-
-// document.querySelector("#form").addEventListener("submit", formSubmit);
-
-// var form = document.querySelector("#form");
-
-// //console.log(form.user.value);
-// // form.id.value
-// function formSubmit(event) {
-//   event.preventDefault();
-
-//   var user = form.email.value;
-//   var pass = form.password.value;
-//   console.log(user, pass);
-
-//   var flag= false
-//   for (var i = 0; i < regdUsers.length; i++) {
-//     if (regdUsers[i].user == user && regdUsers[i].password == pass) {
-//       flag=true;
-//     } else {
-//       flag=false;
-//     }
-
-//   }
-//   if (flag==true){
-//     alert("login Successfull")
-//     window.location.href = "index.html";
-//   }
-//   if(flag==false){
-//     alert("Login fail")
-//   }
-// }
